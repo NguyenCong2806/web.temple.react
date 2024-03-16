6ac8864f6e99eebcb3a77f3a133a54a8715eea20
vancong <108638136+NguyenCong2806@users.noreply.github.com>
2024-03-16 12:24:46 +0700

    upcode

--- src/components/articleheader/articleheader.tsx
+++ src/components/articleheader/articleheader.tsx
@@ -1,9 +1,7 @@
-import { Box, Center, Container, Stack, Text } from "@chakra-ui/react";
+import { Box, Container, Stack, Text,Divider } from "@chakra-ui/react";
 import "./articleheader.scss";
 import { useEffect } from "react";
 import AOS from "aos";
-import "aos/dist/aos.css";
-
 const articleheader = () => {
     useEffect(() => {
         AOS.init();
@@ -11,24 +9,29 @@ const articleheader = () => {
   return (
     <>
       <Box>
-        <Container maxW={"container.xl"} p={"0px"}>
+        <Container maxW={"container.xl"} p={"0px"} data-aos="fade-up">
           <Stack m={"40px 0px"} direction={"column"} spacing={"20px"} textAlign={"center"}>
             <Text
              fontSize={{ base: "10", md: "35", lg: "45" }}
              fontWeight={"400"}
+             color={"rgb(66, 71, 76)"}
             >NEU E-LEARNING</Text>
             <Text
             fontSize={{ base: "10", md: "35", lg: "45" }}
-            fontWeight={"Bold"}
+            fontWeight={"700"}
+            color={"rgb(66, 71, 76)"}
             >CHƯƠNG TRÌNH ĐÀO TẠO TRỰC TUYẾN</Text>
             <Text fontSize={{ base: "10", md: "20", lg: "22" }}
+            color={"rgb(66, 71, 76)"}
             fontWeight={"300"}>
               Chương trình học Online 100%, tốt nghiệp được nhận Bằng Cử Nhân do
               Đại Học Kinh Tế Quốc Dân cấp, được Bộ GD&ĐT công nhận. Đăng ký
               nhận thông tin tư vấn hoàn toàn Miễn Phí
             </Text>
           </Stack>
+          <Divider m={"10px 0px"} orientation='horizontal' />
         </Container>
+       
       </Box>
     </>
   );
--- src/views/layouts/main/main.tsx
+++ src/views/layouts/main/main.tsx
@@ -1,8 +1,10 @@
 import Articleheader from "../../../components/articleheader/articleheader";
+import Cardicon from "../../../components/cardicon/cardicon";
 import Carousel from "../../../components/carousel/carousel";
 import Footer from "../footer/footer";
 import Header from "../header/header";
 import "./main.scss";
+import "aos/dist/aos.css";
 import { Outlet } from "react-router-dom";
 
 const main = () => {
@@ -11,6 +13,7 @@ const main = () => {
       <Header />
       <Carousel/>
       <Articleheader/>
+      <Cardicon/>
       <Outlet />
       <Footer />
     </>
